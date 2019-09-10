16a5533256405469280cca94e28946379a7d16a0
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2019-09-10 16:13:14 +0300

    commit interpolate and identify.endpoints.NA after unit test

--- R/Process_function.R
+++ R/Process_function.R
@@ -70,6 +70,65 @@ pca_reduction = function(data, time_index = TRUE,
 }
 
 
+#' This function identifies consequitive NA sequences
+#' at the beginning or end of data vector. This is an auxilary function
+#' for interpolate
+#'
+#' @param data_vec
+#'
+identify.endpoints.NA = function(data_vec){
+
+  na_ind = which(is.na(data_vec))
+
+  if(length(na_ind) == 0 | length(na_ind) == length(data_vec)){return(NULL)}
+
+  na_list = list()
+
+  # Check for start NA
+
+  if(min(na_ind) == 1){
+
+    end_start_seq_ind = suppressWarnings(min(which(diff(na_ind) > 1)))
+
+    if(is.finite(end_start_seq_ind)){
+
+      na_list$start_indices = na_ind[1:end_start_seq_ind]
+
+    } else
+
+    {na_list$start_indices = na_ind}
+
+
+
+
+  }
+
+  # Check for end NA
+
+  if(max(na_ind) == length(data_vec)){
+
+    rev_na_ind = rev(na_ind)
+
+    start_end_seq_ind = suppressWarnings(length(na_ind) -
+      min(which(diff(rev_na_ind) < -1)) + 1)
+
+    if(is.finite(start_end_seq_ind)){
+
+      na_list$end_indices = na_ind[start_end_seq_ind:length(na_ind)]
+
+    } else
+
+      {na_list$end_indices = na_ind}
+
+
+  }
+
+
+  return(na_list)
+
+}
+
+
 #' This function interpolates forward by using linear interpolation
 #'
 #' @param data_vec
@@ -78,27 +137,49 @@ pca_reduction = function(data, time_index = TRUE,
 #'
 interpolate = function(data_vec, direction = "forward"){
 
+  na_ind = which(is.na(data_vec))
+
   #Check for missing values, return the data if there are no NA
 
-  if(!anyNA(data_vec)){return(data_vec)}
+  if(length(na_ind) == 0){return(data_vec)}
 
-  # Interpolate
+  na_endpoints_list = identify.endpoints.NA(data_vec)
+
+  clean_data = na.approx(data_vec)
+
+  head_len = suppressWarnings(max(na_endpoints_list$start_indices) -
+                                min(na_endpoints_list$start_indices) + 1)
+
+  if(is.infinite(head_len)){head_len = 0}
+
+  tail_len = suppressWarnings(max(na_endpoints_list$end_indices) -
+                                min(na_endpoints_list$end_indices) + 1)
+
+  if(is.infinite(tail_len)){tail_len = 0}
+
 
   if(direction == "forward"){
 
-    clean_vec = na.approx(data_vec)
+    final_point = clean_data[length(clean_data)]
 
-    vec_len = length(data_vec)
+    out_vec = c(rep(NA,head_len),clean_data,rep(final_point, tail_len))
 
-    clean_vec_len = length(clean_vec)
+  } else if (direction == "backward"){
 
-    out_vec = c(rep(NA, vec_len - clean_vec_len), clean_vec)
+    start_point =  clean_data[1]
 
-    return(out_vec)
+    out_vec = c(rep(start_point,head_len),clean_data,rep(NA, tail_len))
 
+  } else if (direction == "both"){
+
+    start_point =  clean_data[1]
+
+    final_point = clean_data[length(clean_data)]
+
+    out_vec = c(rep(start_point,head_len),clean_data,
+                rep(final_point, tail_len))
   }
 
-
-
+  return(out_vec)
 
 }
