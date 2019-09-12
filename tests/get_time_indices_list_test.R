2d36ee4123b80328a27c692f6d04216f9034b327
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2019-09-12 12:53:08 +0300

    commit all files to ensure everything is staged

---
+++ tests/get_time_indices_list_test.R
@@ -0,0 +1,13 @@
+
+dates_vec = seq.Date(from = as.Date("2000-01-01"),
+                     to = as.Date("2005-01-01"),
+                     by = "year")
+
+df = data.frame(Date = dates_vec,
+                X = c(1,2,3,NA,5,6),
+                Y = c(1,2,NA,4,5,6),
+                Z = c(1,2,3,NA,5,6))
+
+dates_list = list(dates_vec[!is.na(df$X)], dates_vec[!is.na(df$Y)])
+
+testthat::expect_equal(get.time.indices.list(df), dates_list)
