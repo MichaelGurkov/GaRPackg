1e881e68c443a0682612a66d58a878eab30f72f2
MichaelGurkov <Michael.Gurkov@mail.huji.ac.il>
2020-07-24 18:48:34 +0300

    add pls_reduction, UNTESTED

--- tests/testthat/test_pls_reduction.R
+++ tests/testthat/test_pls_reduction.R
@@ -2,18 +2,17 @@ date_vec = seq.Date(from = as.Date("2000-01-01"),
                     by = "days", length.out = 12)
 
 test_df = data.frame(date = date_vec,
-                     x = c(NA,1:10,NA), y = rep(22,12) + rnorm(12), z = c(NA,10:1,NA))
+                     x = c(NA,1:10,NA),
+                     y = rep(22,12) + rnorm(12),
+                     z = c(NA,10:1,NA))
 
-result_df = pls_reduction(df = test_df,center = FALSE, scale = FALSE)
+result_df = pls_reduction(df = test_df,
+                          target_var_name = "y",
+                          center = FALSE, scale = FALSE)
 
-test_that("pca reduction returns complete obs date vector",
+test_that("pls reduction returns complete obs date vector",
           expect_equal(
             object = result_df$time_index,
             expected = date_vec[c(-1,-12)]
           ))
 
-test_that("pca reduction returns complete obs pca",
-          expect_equal(
-            object = abs(result_df$pca_obj$x[,1]),
-            expected = test_df$x[c(-1,-12)]
-          ))
